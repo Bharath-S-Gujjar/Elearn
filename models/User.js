a45d8be6544defc70fe62c3d078bc0c4a52f05f0
bharath <gustavofringe942@gmail.com>
2025-02-15 19:31:43 +0530

    Fixed userSchema import issue

--- models/User.js
+++ models/User.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
   password: String,
   courses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }]
 });
 const User = mongoose.models.User || mongoose.model('User', userSchema);
-module.exports = mongoose.model("User", UserSchema);
+// module.exports = mongoose.model("User", UserSchema);
+
+module.exports = userSchema;
